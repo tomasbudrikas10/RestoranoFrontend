28383ca6a79f4243f8e0c985ab05987c6c304d42
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2024-04-18 23:20:51 +0300

    Create Add To Cart Function And Pass It To Products

--- src/ProductMenu.jsx
+++ src/ProductMenu.jsx
@@ -1,6 +1,6 @@
 import Product from "./Product.jsx";
 
-function ProductMenu({products}) {
+function ProductMenu({products, cart, setCart}) {
     const productMenuStyle = {
         display: "flex",
         background: "whitesmoke",
@@ -12,11 +12,15 @@ function ProductMenu({products}) {
         minHeight: "70vh",
         minWidth: "500px",
     }
+    function addToCart(id, name) {
+        setCart({items: [...cart.items, {id: id, name: name}]})
+        localStorage.setItem("cart", JSON.stringify({items: [...cart.items, {id: id, name: name}]}))
+    }
     return <div style={{margin: "50px auto", width: "60%"}}>
         <h1 style={{fontSize: "48px", textAlign: "center", marginBottom: "50px"}}>Tomo Skanusis Meniu</h1>
         <div style={productMenuStyle}>
             {products.map(product => {
-                return product.isAvailable ? <Product key={product.id} product={product}/> : ""
+                return product.isAvailable ? <Product key={product.id} product={product} addToCart={() => addToCart(product.id, product.name)}/> : ""
             })}
         </div
     ></div>
