cd41684b5738f20a50dac3d9958e5a8257f51b9d
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2024-04-18 21:13:12 +0300

    Update Navigation Bar Styling

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,10 +1,36 @@
-function NavigationBar({isLoggedIn}) {
+function NavigationBar(isLoggedIn) {
     const containerStyle = {
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: "10px 100px",
         backgroundColor: "orange",
     }
+    const navigationButtonStyle = {
+        padding: "20px 50px",
+        backgroundColor: "white",
+        border: "2px solid darkorange",
+        fontSize: "20px",
+    }
+    const navigationButtonGroup = {
+        display: "flex",
+        gap: "20px",
+    }
+    const navigationLogoText = {
+        color: "white",
+        fontSize: "36px",
+        fontWeight: "bold",
+        fontFamily: "Arial"
+    }
     return <div style={containerStyle}>
-        <a>Tomo Restoranas</a>
-        {isLoggedIn ? <div><a>Log In</a><a>Register</a></div> : <div><a>Profile</a><a>Log Out</a></div>}
+        <a style={navigationLogoText}>Tomo Restoranas</a>
+        {isLoggedIn ? <div style={navigationButtonGroup}>
+            <a style={navigationButtonStyle}>Log In</a>
+            <a style={navigationButtonStyle}>Register</a>
+        </div> : <div style={navigationButtonGroup}>
+            <a style={navigationButtonStyle}>Profile</a>
+            <a style={navigationButtonStyle}>Log Out</a>
+        </div>}
     </div>
 }
 
