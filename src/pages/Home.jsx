23ada7550a1d995155bdd7b89985b016016efeb7
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2024-04-19 00:00:59 +0300

    Update Home To Have Product Menu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
-function Home() {
-    return <div>home</div>
+import Product from "../Product.jsx";
+import ProductMenu from "../ProductMenu.jsx";
+
+function Home({products, setCart, cart}) {
+    return <ProductMenu cart={cart} products={products} setCart={setCart}/>
 }
 
 export default Home
