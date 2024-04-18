c4458add01e8ef8761ca62782627e567cb486780
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2024-04-18 23:59:47 +0300

    Add Footer Component

---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+function Footer() {
+    return <div style={{background: "lightgray", position: "absolute", bottom: "0", width: "100%", padding: "30px", textAlign: "center"}}>Tomas Budrikas - 2024&copy;</div>
+}
+
+export default Footer
