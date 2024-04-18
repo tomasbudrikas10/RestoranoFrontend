3ae7851db3623da7b4c2c6e46a84e047a6cf7c3a
tomasbudrikas10 <82083897+tomasbudrikas10@users.noreply.github.com>
2024-04-18 20:30:22 +0300

    Load data from server

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,40 @@
+import {useEffect, useState} from "react";
+
 function App() {
+    const [products, setProducts] = useState([])
+    const [orders, setOrders] = useState([])
+    const [orderItems, setOrderItems] = useState([])
+    const [orderStates, setOrderStates] = useState([])
+    const [roles, setRoles] = useState([])
+    const [users, setUsers] = useState([])
+    const [userPaymentMethods, setUserPaymentMethods] = useState([])
+    const dataToLoad = [
+        { endpoint: "products", stateSetter: setProducts },
+        { endpoint: "orders", stateSetter: setOrders },
+        { endpoint: "orderItems", stateSetter: setOrderItems },
+        { endpoint: "orderStates", stateSetter: setOrderStates },
+        { endpoint: "roles", stateSetter: setRoles },
+        { endpoint: "users", stateSetter: setUsers },
+        { endpoint: "userPaymentMethods", stateSetter: setUserPaymentMethods }
+    ];
+
+    useEffect( () => {
+        const loadData = () => {
+            dataToLoad.forEach(item => {
+                fetch(`http://localhost:3000/${item.endpoint}`)
+                    .then(res => res.json())
+                    .then(json => {
+                        if ('data' in json) {
+                            item.stateSetter(json.data)
+                        } else {
+                            console.log("No data property on response.")
+                        }
+                    })
+                    .catch(err => console.log(err))
+            })
+        }
+        loadData();
+    }, [])
   return (
     <div>
       <p>Hello World!</p>
